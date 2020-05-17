40b7b3c2fac664306822a7f58d0da071d2017101
Corwin Buzzard <corwin1998@gmail.com>
2020-05-17 18:23:22 -0400

    tweaks

--- functions/src/api_fetch.ts
+++ functions/src/api_fetch.ts
@@ -1,4 +1,5 @@
 import fetch, { Response } from 'node-fetch';
+import {getKey} from './api_key'
 
 export interface myJson {
     timestamp: number,
@@ -8,7 +9,7 @@ export interface myJson {
 }
 
 export async function getMaxWaves(): Promise<Array<myJson>>{
-    const data: Response = await fetch('http://magicseaweed.com/api/bea1c287853de990886e3599274b5f07/forecast/?spot_id=60&fields=timestamp,swell.absMaxBreakingHeight');
+    const data: Response = await fetch(`http://magicseaweed.com/api/${getKey()}/forecast/?spot_id=60&fields=timestamp,swell.absMaxBreakingHeight`);
     const newData: Array<myJson> = await data.json();
     return newData;
 }
