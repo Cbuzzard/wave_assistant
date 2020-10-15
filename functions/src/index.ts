222d83f2d1fad3f66d23bbbd989f306b0dee5dd5
cbuzzard <corwin1998@gmail.com>
2020-10-15 17:28:43 -0400

    removed dialogflow incompatibale charts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,78 +1,46 @@
 import * as functions from 'firebase-functions';
-import {getHighestWaveResponse, getChartUrl, getHighestWaveEachDayResponse, getWavesOverThreeFeetResponse, getInitialResponse, getTodayWaveResponse, getChartHighestWaveEachDayUrl} from './format_data';
-import {dialogflow, SimpleResponse, Image, BasicCard} from 'actions-on-google';
+import {getHighestWaveResponse, getHighestWaveEachDayResponse, getWavesOverThreeFeetResponse, getInitialResponse, getTodayWaveResponse} from './format_data';
+import {dialogflow, SimpleResponse} from 'actions-on-google';
 
 const app = dialogflow({ debug: true });
 
 app.intent('how are the waves', async (conv) => {
     
     const response: string = await getInitialResponse();
-    const chart: string = await getChartUrl();
     
     conv.ask(new SimpleResponse({
         text: response,
         speech: response
     }));
-
-    conv.ask(new BasicCard({
-        image: new Image({
-            url: chart,
-            alt: 'BuzzardsView.com'
-        })
-    }))
 });
 
 app.intent('waves each day', async (conv) => {
     const waves = await getHighestWaveEachDayResponse();
-    const chart = await getChartHighestWaveEachDayUrl();
 
     conv.close(new SimpleResponse({
         text: waves,
         speech: waves
     }));
-
-    conv.close(new BasicCard({
-        image: new Image({
-            url: chart,
-            alt: 'BuzzardsView.com'
-        })
-    }))
 
 });
 
 app.intent('best waves', async (conv) => {
     const waves = await getWavesOverThreeFeetResponse();
-    const chart = await getChartHighestWaveEachDayUrl();
 
     conv.close(new SimpleResponse({
         text: waves,
         speech: waves
     }));
-
-    conv.close(new BasicCard({
-        image: new Image({
-            url: chart,
-            alt: 'BuzzardsView.com'
-        })
-    }))
 
 });
 
 app.intent('best wave', async (conv) => {
     const waves = await getHighestWaveResponse();
-    const chart = await getChartHighestWaveEachDayUrl();
 
     conv.close(new SimpleResponse({
         text: waves,
         speech: waves
     }));
-
-    conv.close(new BasicCard({
-        image: new Image({
-            url: chart,
-            alt: 'BuzzardsView.com'
-        })
-    }))
 
 });
 
